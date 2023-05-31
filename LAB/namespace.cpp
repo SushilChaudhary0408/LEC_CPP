3b2b5d375812ac4fa59498bf53c763a8dfee24e2
Sushil Chaudhary <sushilchaudhary0408@gmail.com>
2023-05-31 14:05:22 +0545

    commit

---
+++ LAB/namespace.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+namespace BEI{
+char subject[20] = "Electronics";
+int room = 401;
+void printSubject()
+{
+    std::cout<<subject<<std::endl;
+}
+}
+void printRoom(){
+    std::cout<<"room"<<std::endl;
+}
+namespace BCT{
+    char subject[30] = "Software Engineering";
+    int labRoom = 401;
+void printSubject(){
+std::cout<<subject<<endl;
+}
+void printLabRoom(){
+std::cout<<labRoom<<std::endl;
+}
+}
+int main()
+{
+using namespace BEI;
+using namespace BCT;
+printLabRoom();
+printRoom();
+BEI::printSubject();
+BCT::printSubject();
+return 0;
+}
---
+++ LAB/swappointer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int swap(int *s, int *u){
+    int t;
+    cout<<"Initial values\nX="<<*s<<" Y="<<*u;
+    t=*s;
+    *s=*u;
+    *u=t;
+    return 0;
+}
+int getnum(){
+    int a;
+    cout<<"Enter a number";
+    cin>>a;
+    return a;
+}
+int main(){
+    int x=getnum();
+    int y=getnum();
+    swap(&x,&y);
+    cout<<"\nSwapped Values\nX="<<x<<" Y="<<y;
+    return 0;
+}
